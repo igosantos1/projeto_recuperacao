9e75f30c4cde122339c5958cdea34a8ab5bf1634
igosantos <igosantos1@gmail.com>
2024-09-27 21:48:19 -0300

    recuperacao

---
+++ src/app/produtos.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProdutosService {
+
+  constructor(private http: HttpClient) { }
+
+  pegarProdutos(): Observable<any>  {
+    return this.http.get("http://localhost:3000/produtos")
+  }
+}
---
+++ src/app/produtos/produtos.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { ProdutosService } from '../produtos.service';
+
+@Component({
+  selector: 'app-produtos',
+  templateUrl: './produtos.component.html',
+  styleUrls: ['./produtos.component.css']
+})
+export class ProdutosComponent {
+
+  listaProdutos: any[] = []
+
+  constructor(private produtoService: ProdutosService) {}
+
+  ngOnInit() {
+    this.produtoService.pegarProdutos().subscribe(
+      produtos => {
+        this.listaProdutos = produtos
+      }
+    )
+
+  }
+
+}
